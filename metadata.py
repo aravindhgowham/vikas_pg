5c125ae816556df29eeafd47b898541ad0ecaf13
arvind <arvind@EZIO.COM>
2026-01-29 17:36:51 +0530

    intial push

---
+++ metadata.py
@@ -0,0 +1,4 @@
+__author__ = "Aravindh"
+__author_email__ ="aravindhvignesh58@gmail.com"
+__github__ = "https://github.com/aravindhgowham"
+__description__ = "High-performance PostgreSQL utility with vikas_pg lib. Async PostgreSQL utilities (Pydantic v2 compatible)"
